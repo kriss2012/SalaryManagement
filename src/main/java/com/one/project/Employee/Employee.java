a4b472746f440ba1bf495f84e7122ace032dc7b3
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-07-22 21:45:11 +0530

    Update Employee.java

--- src/main/java/com/one/project/Employee/Employee.java
+++ src/main/java/com/one/project/Employee/Employee.java
@@ -9,10 +9,16 @@ public class Employee {
 	private int id;
 	
 	private String name;
-	
 	private String designation;
-	
 	private double Salary;
+	
+	// Anime character enhancements & Credentials
+	private String password;
+	private String animeAlias;
+	private String avatar; // e.g. "naruto", "luffy", "gojo", "nezuko"
+	private int powerLevel;
+	private String magicElement; // e.g. "Fire", "Lightning", "Shadow", "Spirit"
+	private String specialAbility; // e.g. "Rasengan", "Domain Expansion"
 
 	public int getId() {
 		return id;
@@ -46,19 +52,69 @@ public class Employee {
 		Salary = salary;
 	}
 
-	public Employee(int id, String name, String designation, double salary) {
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAnimeAlias() {
+		return animeAlias;
+	}
+
+	public void setAnimeAlias(String animeAlias) {
+		this.animeAlias = animeAlias;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public int getPowerLevel() {
+		return powerLevel;
+	}
+
+	public void setPowerLevel(int powerLevel) {
+		this.powerLevel = powerLevel;
+	}
+
+	public String getMagicElement() {
+		return magicElement;
+	}
+
+	public void setMagicElement(String magicElement) {
+		this.magicElement = magicElement;
+	}
+
+	public String getSpecialAbility() {
+		return specialAbility;
+	}
+
+	public void setSpecialAbility(String specialAbility) {
+		this.specialAbility = specialAbility;
+	}
+
+	public Employee(int id, String name, String designation, double salary, String password, String animeAlias, String avatar, int powerLevel, String magicElement, String specialAbility) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.designation = designation;
-		Salary = salary;
+		this.Salary = salary;
+		this.password = password;
+		this.animeAlias = animeAlias;
+		this.avatar = avatar;
+		this.powerLevel = powerLevel;
+		this.magicElement = magicElement;
+		this.specialAbility = specialAbility;
 	}
 
 	public Employee() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-	
-	
-
 }
